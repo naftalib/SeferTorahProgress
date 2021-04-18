4a0c4fa9d48124f3254cecf9e4d427caad59508a
naftalib <45719650+naftalib@users.noreply.github.com>
2021-04-19 02:00:02 +0300

    titles added

--- src/data/titles.js
+++ src/data/titles.js
@@ -15,27 +15,27 @@ const Titles = [
     },
      {
         id:4,
-        Title: 'Amud: 3',
+        Title: 'Amud: 4',
     },
      {
         id:5,
-        Title: 'Amud: 3',
+        Title: 'Amud: 5',
     },
      {
         id:6,
-        Title: 'Amud: 3',
+        Title: 'Amud: 6',
     },
      {
         id:7,
-        Title: 'Amud: 3',
+        Title: 'Amud: 7',
     },
      {
         id:8,
-        Title: 'Amud: 3',
+        Title: 'Amud: 8',
     },
      {
         id:9,
-        Title: 'Amud: 3',
+        Title: 'Amud: 9',
     }
 
 
