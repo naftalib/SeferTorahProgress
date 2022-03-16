3ef3638cb4ec74fa64aecab7b14758831abb4304
naftalib <45719650+naftalib@users.noreply.github.com>
2022-03-16 02:25:46 +0200

    progress

--- src/components/Amud.js
+++ src/components/Amud.js
@@ -5,19 +5,16 @@ const Amud = (props)=>{
   
   return (
 
-   <div>
+   <div className='tileCard'>
       <img src={img} alt="amud" 
       //conditional render to define class name - determines if scalable or not
       className={!props.tile.complete ? 'amud':'amud scale'}
       //condit render defines opacity
       style={props.tile.complete ? {opacity: '1'}: {opacity: '0.5'}}
       />
-      <div className='amudTitle'>
-        <h2>{props.tile.title}</h2>
-        <hr className='div-line'/>
+        <h2 className='amudTitle'>{props.tile.title}</h2>
+        {/* <hr className='div-line'/> */}
         <h3>status: {props.tile.complete ? "complete":"pending"}</h3> 
-      </div>
-        
   </div>
   
   )
